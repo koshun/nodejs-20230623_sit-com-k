34a27dd4e49cab32aea4c1d9a267eb00ad9d09fa
koshun <sit-com.k@yandex.ru>
2023-07-10 16:03:30 +0300

    as

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,11 +4,18 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.limit = options.limit;
+    this.size = 0;
   }
 
   _transform(chunk, encoding, callback) {
+   this.size += chunk.length;
 
+    if (this.size > this.limit) {
+      callback(new LimitExceededError());
+    } else {
+      callback(null, chunk);
+    }
   }
 }
-
 module.exports = LimitSizeStream;
