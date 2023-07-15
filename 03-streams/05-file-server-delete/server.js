71fd8baf355afcf692f2870c53079fabf3990380
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-07-15 13:43:42 +0300

    Added solution for task: Файловый сервер - удаление файла

--- 03-streams/05-file-server-delete/server.js
+++ 03-streams/05-file-server-delete/server.js
@@ -2,16 +2,31 @@ const url = require('url');
 const http = require('http');
 const path = require('path');
 
+const removeFile = require('./removeFile');
+
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
+  if (pathname.includes('/') || pathname.includes('..')) {
+    res.statusCode = 400;
+    res.end('Nested paths are not allowed');
+    return;
+  }
+
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'DELETE':
+      if (!filepath) {
+        res.statusCode = 404;
+        res.end('File not found');
+        return;
+      }
+
+      removeFile(filepath, res);
 
       break;
 
