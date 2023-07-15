9967ab4c776bf4346273ec5ab7738fe5b55c6b72
javascriptru <67475495+jsru-1@users.noreply.github.com>
2023-07-15 13:35:07 +0300

    Added solution for task: Файловый сервер - отдача файла

--- 03-streams/03-file-server-get/server.js
+++ 03-streams/03-file-server-get/server.js
@@ -2,16 +2,25 @@ const url = require('url');
 const http = require('http');
 const path = require('path');
 
+const sendFile = require('./sendFile');
+
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const url = new URL(req.url, `http://${req.headers.host}`);
   const pathname = url.pathname.slice(1);
 
+  if (pathname.includes('/') || pathname.includes('..')) {
+    res.statusCode = 400;
+    res.end('Nested paths are not allowed');
+    return;
+  }
+
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
+      sendFile(filepath, res, req);
 
       break;
 
